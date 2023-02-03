c2c021cbc67928f9e266f263786d44975f403fc7
phillipstm <luvmargaritas@gmail.com>
2023-02-03 16:31:00 -0600

    changed file name from in-transit to driverDelivered and removed vendor file

--- hub.js
+++ hub.js
@@ -2,12 +2,16 @@
 
 const eventPool = require('./eventPool');
 const chance = require('./src/modules/chance');
-const { driverHandler } = require('./src/modules/driver');
-const { deliveredHandler, vendorPickup } = require('./src/modules/vendorPickup');
+const { driverHandler } = require('./src/handlers/driver');
+const { deliveredHandler } = require('./src/handlers/vendor');
+const { vendorPickup } = require('./src/modules/vendorPickup');
+const { driverDelivered } = require('./src/handlers/driverDelivered')
+
 
 class EventLog {
   constructor(event, payload) {
     this.event = event;
+    this.date = new Date();
     this.time = new Date.toTimeString();
     this.payload = payload;
   }
@@ -16,10 +20,14 @@ class EventLog {
   }
 }
 
-eventPool.on('pickup', (payload) => new EventLog('pickup', payload).log());
-eventPool.on('pickup', driverHandler);
+eventPool.on('Vendor pickup', () => new EventLog('pickup', vendorPickup.payload).log());
+eventPool.on(`Driver pickup`, driverHandler);
 eventPool.on('in-transit', (payload) => new EventLog('in-transit', payload).log());
-eventPool.on('delivered', (payload) => new EventLog('delivered', payload).log());
+eventPool.on('driverDelivered', (payload) => new EventLog('driverDelivered', payload).log());
 eventPool.on('delivered', deliveredHandler);
 
-vendorPickup(chance.company());
+setInterval(() => {
+  const vendorPickup = (chance.orderId())
+  console.log(`Order has changed ${vendorPickup}`);
+  eventPool.emit('Vendor pickup', { vendorPickup})
+}, 5000)
