9891681668de3db7e46dea5d08b62e729bc2e9b5
phillipstm <luvmargaritas@gmail.com>
2023-02-03 18:21:00 -0600

    updated vendor pickup

--- src/handlers/vendorPickup.js
+++ src/handlers/vendorPickup.js
@@ -1,19 +1,14 @@
 'use strict';
 
 const eventPool = require('../eventPool');
-const chance = require('../modules/chance');
 
-let vendorPickup = (payload) => {
-  let payload = {
-    store: chance.company(),
-    orderID: chance.guid(),
-    customer: chance.name(),
-    address: chance.address({short_suffix: true}),
-  };
+module.exports = (payload) => {
+setTimeout (() => {
   console.log(`VENDOR: ready for pickup ${payload.store}`);
   eventPool.emit('VENDOR PICKUP', payload);
+}, 3000);
 };
 
 
-module.exports = {vendorPickup};
+
   
